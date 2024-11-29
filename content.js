cec0bd03d96b711fa5ae34cd9ca829de07a7bde7
Kumar Nirupam <kumar.nirupam24@gmail.com>
2024-11-29 11:06:58 +0530

    changes in content.jd

--- content.js
+++ content.js
@@ -1,1 +1,6 @@
 console.log("Content script running on this page.");
+// Extract main content (you can refine this based on the webpage structure)
+const content = document.body.innerText;  // You could use more targeted selectors depending on the page
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    sendResponse({ content: content });
+});
